e87a317604fe844133bc379b860344e2bf9e804c
dzp62442 <dzp62442@163.com>
2023-05-03 15:24:25 +0800

    参数调试

--- examples/MyRobot/main.cpp
+++ examples/MyRobot/main.cpp
@@ -29,84 +29,70 @@ typedef MyRobot::MeasurementModel<T> MeasurementModel;
 
 int main(int argc, char** argv)
 {
-    // 状态向量真实值（无噪声）
+    // 状态向量真实值
     State x;
     x.setZero();
     
-    // Control input
-    Control u;
-    // System model
-    SystemModel sys;
+    Control u;  // 控制输入
+    SystemModel sys;  // 系统模型
+    MeasurementModel mm;  // 观测模型
     
-    // Measurement model
-    MeasurementModel mm;
-    
-    // Random number generation (for noise simulation)
+    // 生成随机噪声
     std::default_random_engine generator;
     generator.seed( std::chrono::system_clock::now().time_since_epoch().count() );
-    std::normal_distribution<T> noise(0, 1);
+    std::normal_distribution<T> noise(0, 1);  // 服从均值为0，标准差为1的正态分布
     
-    // Some filters for estimation
-    // Pure predictor without measurement updates
+    // 纯预测器，没有观测更新
     Kalman::ExtendedKalmanFilter<State> predictor;
-    // Extended Kalman Filter
+    // 扩展卡尔曼滤波器
     Kalman::ExtendedKalmanFilter<State> ekf;
-    // Unscented Kalman Filter
-    Kalman::UnscentedKalmanFilter<State> ukf(1);
     
-    // Init filters with true system state
+    // 初始化滤波器
     predictor.init(x);
     ekf.init(x);
-    ukf.init(x);
     
-    // Standard-Deviation of noise added to all state vector components during state transition
-    T systemNoise = 0.01;
-    // Standard-Deviation of noise added to all measurement vector components in orientation measurements
-    T orientationNoise = 0.025;
-    // Standard-Deviation of noise added to all measurement vector components in distance measurements
-    T distanceNoise = 0.025;
+    T systemNoise = 0.1;  // 状态向量的噪声标准差
+    T orientationNoise = 0.05;  // 观测向量的朝向分量的噪声标准差
+    T distanceNoise = 0.05;  // 观测向量的距离分量的噪声标准差
     
     // Simulate for 100 steps
     const size_t N = 100;
     for(size_t i = 1; i <= N; i++)
     {
         // 控制向量输入
+        // u.v() = 1. + std::sin( T(2) * T(M_PI) / T(N) );
+        // u.dtheta() = std::sin( T(2) * T(M_PI) / T(N) ) * (1 - 2*(i > 50));
         u.v() = 1. + std::sin( T(2) * T(M_PI) / T(N) );
-        u.dtheta() = std::sin( T(2) * T(M_PI) / T(N) ) * (1 - 2*(i > 50));
+        u.dtheta() = std::sin( T(2) * T(M_PI) / T(N) );
         
-        // Simulate system
+        // 仿真系统模型
         x = sys.f(x, u);
         
-        // Add noise: Our robot move is affected by noise (due to actuator failures)
+        // 引入过程噪声Wk
         x.x() += systemNoise*noise(generator);
         x.y() += systemNoise*noise(generator);
         x.theta() += systemNoise*noise(generator);
         
-        // Predict state for current time-step using the filters
+        // 预测当前时刻系统状态
         auto x_pred = predictor.predict(sys, u);
         auto x_ekf = ekf.predict(sys, u);
-        auto x_ukf = ukf.predict(sys, u);
 
         {
-            // We can measure the position every 10th step
+            // 仿真观测模型
             Measurement measure = mm.h(x);
             
-            // Measurement is affected by noise as well
+            // 引入观测噪声Vk
             measure.d() += distanceNoise * noise(generator);
             measure.alpha() += orientationNoise * noise(generator);
             
-            // Update EKF
+            // 更新EKF
             x_ekf = ekf.update(mm, measure);
-            
-            // Update UKF
-            x_ukf = ukf.update(mm, measure);
         }
         
-        // Print to stdout as csv format
+        // 以csv格式打印输出
         std::cout   << x.x() << "," << x.y() << "," << x.theta() << ","
                     << x_pred.x() << "," << x_pred.y() << "," << x_pred.theta()  << ","
-                    << x_ekf.x() << "," << x_ekf.y() << "," << x_ekf.theta()  << ","
-                    << x_ukf.x() << "," << x_ukf.y() << "," << x_ukf.theta()
+                    << x_ekf.x() << "," << x_ekf.y() << "," << x_ekf.theta()
                     << std::endl;
     }
     
--- examples/MyRobot/visualization.m
+++ examples/MyRobot/visualization.m
@@ -1,19 +1,33 @@
 load('data.csv');
 
-% Plot positions
 figure;
-title('Position Estimate');
-plot(data(:,[1,4,7,10]),data(:,[2,5,8,11]));
-legend({'True Position', 'Prediction only','EKF Estimate', 'UKF Estimate'});
-xlabel('x');
-ylabel('y');
+plot(data(:,[1,4,7]),data(:,[2,5,8]), 'LineWidth', 1);
+title('位置预测结果');
+legend({'位置真值', '直接计算', 'EKF'});
+xlabel('x位置坐标');
+ylabel('y位置坐标');
 
-% Estimation error (euclidian distance)
 figure;
-title('Estimate errors from true position (Euclidian distance)');
+predict_error = sqrt((data(:,1)-data(:,4)).^2 + (data(:,2)-data(:,5)).^2);
 ekf_error = sqrt((data(:,1)-data(:,7)).^2 + (data(:,2)-data(:,8)).^2);
-ukf_error = sqrt((data(:,1)-data(:,10)).^2 + (data(:,2)-data(:,11)).^2);
-plot([ekf_error, ukf_error]);
-legend({'EKF Error', 'UKF Error'});
-xlabel('Iteration')
-ylabel('Error')
+plot([predict_error, ekf_error], 'LineWidth', 1);
+title('位置预测误差（欧氏距离）');
+legend({'直接计算误差', 'EKF误差'});
+xlabel('迭代次数');
+ylabel('欧氏距离误差');
+
+figure;
+plot(data(:,[3,6,9]), 'LineWidth', 1);
+title('朝向角度预测结果');
+legend({'角度真值', '直接计算', 'EKF'});
+xlabel('迭代次数');
+ylabel('朝向角度');
+
+figure;
+predict_err = sqrt((data(:,3)-data(:,6)).^2);
+ekf_err = sqrt((data(:,3)-data(:,9)).^2);
+plot([predict_err, ekf_err], 'LineWidth', 1);
+title('朝向角预测误差（角度）');
+legend({'直接计算误差', 'EKF误差'});
+xlabel('迭代次数');
+ylabel('角度误差');
